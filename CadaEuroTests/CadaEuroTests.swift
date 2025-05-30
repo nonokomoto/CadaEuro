8bb5ead75636eaa75a42f7115a7e5f269a1a94a1
Ney Carvalho <57563700+nonokomoto@users.noreply.github.com>
2025-05-30 16:26:23 +0200

    Initial Commit

---
+++ CadaEuroTests/CadaEuroTests.swift
@@ -0,0 +1,17 @@
+//
+//  CadaEuroTests.swift
+//  CadaEuroTests
+//
+//  Created by NeyCarvalho on 30/05/2025.
+//
+
+import Testing
+@testable import CadaEuro
+
+struct CadaEuroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
